35673edee00477006f20d1469aa8838ec84db9fb
Alexey Pekhov <A.Pekhov@bssys.com>
2018-04-04 15:08:42 +0300

    Тест

--- lib/egg_autotests/ia_ActiveMQ.rb
+++ lib/egg_autotests/ia_ActiveMQ.rb
@@ -1,5 +1,3 @@
-#require "#{Rails.root}/lib/egg_autotests/egg_autotests_list.rb"
-
 class IA_ActiveMQ
 
   def initialize(pass_menu_color, fail_menu_color, not_find_xml, not_receive_answer, egg_version, try_count)

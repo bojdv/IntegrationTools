0fb8abee02e9f64d1433d09aa79413831882dcbe
Pekhov <alexey@pekhov.com>
2018-12-21 10:59:05 +0300

    idea remove ufebs 2019

--- lib/egg_autotests/egg_autotests_list.rb
+++ lib/egg_autotests/egg_autotests_list.rb
@@ -36,7 +36,7 @@ class EggAutotestsList
       when build_version.include?('6.10')
         @ufebs_version = '2018.3.2' #\app\smx\resourceapp.war\wsdl\XSD\CBR\х\ed\cbr_ed101_vх.xsd
       else
-        @ufebs_version = '2018.4.1'
+        @ufebs_version = '2019.1.1'
     end
 
   end
